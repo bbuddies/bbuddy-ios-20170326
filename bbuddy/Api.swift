7cb684dd7e71a00608231ae630d0371385295363
Jackson Zhang <zbcjackson@gmail.com>
2017-02-10 17:57:08 +0800

    Update token from every response

--- bbuddy/Api.swift
+++ bbuddy/Api.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Moya
+import Result
 
 enum Api {
     case signIn(email: String, password: String)
@@ -45,6 +46,16 @@ struct AuthPlugin: PluginType {
         request.addValue(token.client, forHTTPHeaderField: "client")
         request.addValue(token.accessToken, forHTTPHeaderField: "access-token")
         return request
+    }
+    
+    func didReceive(_ result: Result<Response, MoyaError>, target: TargetType) {
+        if let headers = (result.value?.response as? HTTPURLResponse)?.allHeaderFields,
+            let uid = headers["uid"] as? String,
+            let client = headers["client"] as? String,
+            let accessToken = headers["access-token"] as? String,
+            let type = headers["token-type"] as? String {
+            User.save([.token: accessToken, .email: uid, .client: client, .type: type])
+        }
     }
 }
 
