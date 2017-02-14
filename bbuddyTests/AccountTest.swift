47b5ab46bf1b0305847ce3d738c98d850d38928a
Jackson Zhang <zbcjackson@gmail.com>
2017-02-14 11:10:38 +0800

    Add test target and mock

---
+++ bbuddyTests/AccountTest.swift
@@ -0,0 +1,22 @@
+//
+//  AccountTest.swift
+//  bbuddy
+//
+//  Created by Jackson Zhang on 14/02/2017.
+//  Copyright © 2017 odd-e. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import bbuddy
+
+class AccountTest: QuickSpec {
+    override func spec() {
+        describe("Account"){
+            it("add a new account if no id"){
+                var account = Account()
+                
+            }
+        }
+    }
+}
