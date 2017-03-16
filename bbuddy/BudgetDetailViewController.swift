488941ad48348f9a8990caffb24619e9017d4459
Jackson Zhang <zbcjackson@gmail.com>
2017-03-16 15:03:34 +0800

    Add budget ui

---
+++ bbuddy/BudgetDetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  BudgetDetailViewController.swift
+//  bbuddy
+//
+//  Created by Jackson Zhang on 16/03/2017.
+//  Copyright © 2017 odd-e. All rights reserved.
+//
+
+import UIKit
+
+class BudgetDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func saveBudget(_ sender: UIButton) {
+        _ = navigationController?.popViewController(animated: true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
